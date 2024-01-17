115fe0f9dfe90b4827b7d3cef42921177c258dff
Kenny <adegokelisha260@gmail.com>
2024-01-17 13:40:04 +0100

    changing reset password

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,6 @@ urlpatterns =[
     path('register/',RegisterUserAPIView.as_view()),
     path('view/',ListUserView.as_view(),name = 'view'),
     path('update/<int:pk>/', UpdateUserView.as_view(),name = 'update'),
-    path('api/reset_password/', include('django_rest_passwordreset.urls',namespace='reset_password')),
+    path('api/forgot_password/', include('django_rest_passwordreset.urls',namespace='forgot_password')),
 
 ]
